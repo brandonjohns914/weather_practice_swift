c4041fcb63d0e92b1b94ecbf6de7770c077e7301
Brandon Johns <brandonjohns914@yahoo.com>
2022-10-05 14:16:53 -0700

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Brandon Johns on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
